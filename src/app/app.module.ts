41d2f479b7cdd214269029a383bdd2465ca10455
bihi <bihi27.mail@gmail.com>
2017-09-20 13:56:38 +0200

    új route bejegyzés, ami a keresőoldalra irányít át, így feleslegessé vált a navigate az app.components.ts-ben

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {FormsModule} from '@angular/forms';
 import {CartItemNumberService} from './cart-item-number.service';
 
 const appRoutes: Routes = [
+  {path: '', component: SearchResultsComponent},
   {path: 'results', component: SearchResultsComponent},
   {path: 'details', component: BookDetailsComponent},
   {path: 'cart', component: CartComponent}
